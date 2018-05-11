7247b21b5977f102773b28cb542741482cd0e2e0
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-05-11 18:27:30 +0200

    start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import AppPosts from './pages/AppPosts.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/posts'},
+  { path: '/posts', component: AppPosts, name:'all-posts' },
+
+]
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
